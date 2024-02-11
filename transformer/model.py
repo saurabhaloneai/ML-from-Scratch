a2bd39e78b4d797ff3fbc8d552799ede2d26e3ea
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-02-11 19:31:37 +0530

    added the layer-norm

--- transformer/model.py
+++ transformer/model.py
@@ -35,31 +35,56 @@ class postionalembeddinglayer(nn.Module):
         self.dropout = nn.Dropout(dropout)
         
         #matrix for (d_model, seq_len)  
-        
         pe = torch.zeros(d_model, seq_len)
 
         #converting the position into a tesnor 
-        
+        # PE(pos,2i) =sin(pos/100002i/dmodel) -> from paper
+        # PE(pos,2i+1) =cos(pos/100002i/dmodel) i is the dimension
+
         position = torch.arange(0, seq_len, dtype = torch.float).unsqueeze(1)
-        
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         
         # sin to even position and cos to odd position  
-        
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        
-        pe  = pe.unsqueeze(0) # (1,d_model, seq_len)
+       
+        # (1,d_model, seq_len)
+        pe  = pe.unsqueeze(0) 
         
         # pe = position embedding
-        
         self.register_buffer('pe', pe)  # register_buffer TO STORE THE INFO ABOUT 
-                                        #POSTIONAL EMNEDDING IG it will be use at output 
+                                        # POSTIONAL EMEDDING IG it will be use at output 
                 
-        
-    
+                
     def forward(self, x):
         
         x = x + (self.pe[:, :x.size(1)]).requires_grad_(False)
-        
         return self.dropout(x)
+    
+
+
+# layer normailzation 
+
+class layernorm(nn.Module):
+    
+    
+    def __init__(self, eps:float = 1e-6):
+        
+        super().__init__()
+        
+        self.eps = eps
+        self.w = nn.Parameter(torch.ones(1))
+        self.b = nn.Parameter(torch.zeros(1))
+    
+    def forward(self, x): 
+        mean = x.mean(-1, keepdim = True) # u 
+        std = x.std(-1, keepdim = True)
+        return self.w * (x - mean) / (std + self.eps) + self.b
+    
+    
+# feed forward layer 
+# FFN(x) = max(0, xW1 + b1 )W2 + b2   
+
+class feedforwardlayer(nn.Module):
+    
+    pass
