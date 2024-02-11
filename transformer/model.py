33f9263fe4bdc8cd46b8ef7d553cbf88754b8718
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-02-11 19:38:37 +0530

    added the feedforward network

--- transformer/model.py
+++ transformer/model.py
@@ -87,4 +87,18 @@ class layernorm(nn.Module):
 
 class feedforwardlayer(nn.Module):
     
-    pass
+    def __init__(self, d_model:int, d_ff:int, dropout:float):
+        
+        super().__init__()
+        
+        self.d_model = d_model
+        self.d_ff = d_ff
+        
+        self.layer1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.layer2 = nn.Linear(d_ff, d_model)
+        
+    def forward(self, x):
+        return self.layer2(self.dropout(torch.relu(self.layer1(x))))
+    
+    
