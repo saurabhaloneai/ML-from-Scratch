9753a4c222cf3bb69738ab528081167cd7c0f7d2
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-02-19 12:26:18 +0530

    adds the get tokenizers

--- transformer/train.py
+++ transformer/train.py
@@ -17,4 +17,11 @@ def get_all_sentences(ds, lang):
 def get_tokenizer(config, ds , lang):
     tokenizer_path = Path(config['tokenizer_path'].format(lang))
     if not Path.exists(tokenizer_path):
-        tokenizers = Tokenizer(WordLevel(unk_token=[config['unk_token']]))    
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
