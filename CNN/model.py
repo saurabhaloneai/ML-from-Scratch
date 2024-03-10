b39d1298b2b71d0d0ee3ad62e78d7600519fbcf5
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-03-10 18:36:50 +0530

    added the convo for 1D with no errors

--- CNN/model.py
+++ CNN/model.py
@@ -9,17 +9,21 @@ def convo1d(x,w,s=1,p=0):
     x_padded = np.array(x)
 
     if p >0 :
-        zero_pad = np.zeros(shape=0)
+        zero_pad = np.zeros(shape=p)
         x_padded = np.concatenate([zero_pad, x_padded, zero_pad])
         res =[]
 
     for i in range(0, int((len(x_padded)-len(w_rot)))+1,s):
-        res.append(np.sum(x_padded[i:o+w_rot.shape[0]]*w_rot))
+        res.append(np.sum(x_padded[i:i+w_rot.shape[0]]*w_rot))
     return np.array(res)
 
-
-
-
 ##testing 
+
+x = [1,2,3,4,5,6,1,3]
+w = [1,0,3,1,2]
+
+print("convo1D RES : ", convo1d(x,w,p=2,s=1))
+
+print("convo1d numpy: ", np.convolve(x,w,mode='same'))
 
 
