14e7ba8a208cf0ea4771d2ec5534897619326d9e
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-03-13 07:38:09 +0530

    all test pass

--- CNN/model.py
+++ CNN/model.py
@@ -54,31 +54,54 @@ print("convo1d: valid: ",np.convolve(x,w,mode='valid'))
 
 #let's build the convo2d 
 
-def convo2d(x,w,s=(1,1),p=(0,0)):
+# def convo2d(x,w,s=(1,1),p=(0,0)):
 
 
-    w_rot = np.array(w)[::-1,::-1]
-    x_orig = np.array(x)
+#     w_rot = np.array(w)[::-1,::-1]
+#     x_orig = np.array(x)
     
-    n1 = x_orig.shape[0] + 2*p[0]
-    n2 = x_orig.shape[1] + 2*p[1]
+#     n1 = x_orig.shape[0] + 2*p[0]
+#     n2 = x_orig.shape[1] + 2*p[1]
 
-    x_padded = np.zeros(shape = (n1,n2))
+#     x_padded = np.zeros(shape = (n1,n2))
 
-    x_padded[p[0]:p[0]+x_orig.shape[0],p[1]:p[1]+x_orig.shape[1]] = x_orig 
+#     x_padded[p[0]:p[0]+x_orig.shape[0],p[1]:p[1]+x_orig.shape[1]] = x_orig 
 
-    res = []
+#     res = []
 
-    for i in range(0, int((x_padded.shape[0] - w_rot.shape[0])/s[0])+1,s[0]):
+#     for i in range(0, int((x_padded.shape[0] - w_rot.shape[0])/s[0])+1,s[0]):
 
-        res.append([])
+#         res.append([])
 
-        for j in range(0, int((x_padded.shape[1] -  w_rot.shape[1])/s[1])+1,s[1]):
+#         for j in range(0, int((x_padded.shape[1] -  w_rot.shape[1])/s[1])+1,s[1]):
 
-            x_sub = x_padded[i:i+w_rot.shape[0],j:j+w_rot.shape[1]]
-            res[-1].append(np.sum(x_sub*w)) 
+#             x_sub = x_padded[i:i+w_rot.shape[0],j:j+w_rot.shape[1]]
+#             res[-1].append(np.sum(x_sub*w)) 
 
-    return(np.array(res))
+#     return(np.array(res))
+
+
+def conv2d(x, w, s=(1, 1), p=(0, 0)):
+    
+    w  = np.array(w)[::-1,::-1]
+    x = np.array(x)
+    # Pad the input
+    x_padded = np.pad(x, ((p[0], p[0]), (p[1], p[1])), mode='constant')
+
+    # Get the output shape
+    output_height = (x_padded.shape[0] - w.shape[0]) // s[0] + 1
+    output_width = (x_padded.shape[1] - w.shape[1]) // s[1] + 1
+
+    # Initialize the output
+    output = np.zeros((output_height, output_width))
+
+    # Perform the convolution
+    for i in range(output_height):
+        for j in range(output_width):
+            x_sub = x_padded[i * s[0]:i * s[0] + w.shape[0], j * s[1]:j * s[1] + w.shape[1]]
+            output[i, j] = np.sum(x_sub * w)
+
+    return output
 
 
 #lets provide the inputs for convo2d
@@ -88,5 +111,5 @@ x = [[1,3,2,4],[5,6,1,3],[1,2,0,2],[3,4,3,2]]
 
 w = [[1,0,3],[1,2,1],[0,1,1]]
 
-print("convo2d : ",convo2d(x,w,p=(1,1),s=(1,1)))
+print("convo2d : ",conv2d(x,w,p=(1,1),s=(1,1)))
 print("convo2d with scipy : ",scipy.signal.convolve2d(x,w,mode='same'))
